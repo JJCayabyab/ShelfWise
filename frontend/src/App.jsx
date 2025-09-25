b686590d9896d4ba1896f41d5bef5d45917e396d
Jeremy Cayabyab <cayabyabjeremy25@gmail.com>
2025-09-25 16:16:53 +0800

    added theme controller and somes libraries

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,20 @@
-import './index.css'
-function App() {
- 
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import "./index.css";
+import Home from "./pages/Home";
+import BookPage from "./pages/BookPage";
 
+function App() {
   return (
- <>
-<div className="text-4xl">
-Inamo
-</div>
- </>
-  )
+    <div>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/book/:id" element={<BookPage />} />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
