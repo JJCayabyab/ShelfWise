0632211f56860463f200afb32ea01e4ff64c9f14
Jeremy Cayabyab <cayabyabjeremy25@gmail.com>
2025-09-28 00:28:37 +0800

    deployed backend on render for testing

--- frontend/src/store/bookStore.js
+++ frontend/src/store/bookStore.js
@@ -1,6 +1,8 @@
 import { create } from "zustand";
 import axios from "axios";
-const url = "http://localhost:5001";
+
+// const url = "https://shelfwise-1.onrender.com";  // Production
+const url = "http://localhost:5001"; // Development
 
 //for  fetching all books
 export const useBookStore = create((set, get) => ({
@@ -12,6 +14,7 @@ export const useBookStore = create((set, get) => ({
 
     try {
       const res = await axios.get(`${url}/api/books`);
+      console.log(url);
       set({ books: res.data.data, error: null });
     } catch (error) {
       if (error.response && error.response.status === 429) {
